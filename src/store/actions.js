9bdb3e756a04ee7c7f3ca9b959e9afe9b3f9daac
rakeshpasalu <39869927+rakeshpasalu@users.noreply.github.com>
2020-04-06 09:25:56 +0530

    added src and public folder

---
+++ src/store/actions.js
@@ -0,0 +1,10 @@
+const actionTypes = {
+
+    INCREMENT: 'INCREMENT',
+    DECREMENT: 'DECREMENT',
+    SUBTRACT: 'SUBTRACT',
+    ADD: 'ADD',
+    STORE_RESULT: 'STORE_RESULT',
+    DELETE_RESULT: 'DELETE_RESULT'
+}
+export default actionTypes;
